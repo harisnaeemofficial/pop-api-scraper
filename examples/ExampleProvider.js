a0e2a6837be8d3ab88ee2525e69436423e3babe3
Chris Alderson <chrisalderson@protonmail.com>
2017-11-12 19:56:38 +0100

    add provider example

---
+++ examples/ExampleProvider.js
@@ -0,0 +1,47 @@
+// Import the necessary modules.
+// @flow
+import AbstractProvider from '../src/providers/AbstractProvider'
+
+/**
+ * Example provider extending the abstract provider.
+ * @extends {AbstractProvider}
+ * @type {ExampleProvider}
+ */
+export default class ExampleProvider extends AbstractProvider {
+
+  /**
+   * The http service for the provider.
+   * @type {HttpService}
+   */
+  _httpService: HttpService
+
+  /**
+   * Create a nwe AbstractProvider object.
+   * @param {!PopApiScraper} PopApiScraper - The PopApScraper instance.
+   * @param {!Object} options - The options for the AbstractProvider.
+   * @param {!string} options.name - The name of the provider.
+   * @param {!Array<Object>} options.configs - The configurations of the
+   * provider.
+   */
+  constructor(PopApiScraper: PopApiScraper, {name, configs}: Object): void {
+    super(PopApiScraper, {name, configs})
+
+    this._httpService = new HttpService({
+      baseUrl: 'https://jsonplaceholder.typicode.com/'
+    })
+  }
+
+  /**
+   * Get the contents for a configuration.
+   * @override
+   * @param {!Object} config - The config to get content with.
+   * @returns {Promise<Array<Object>, Error>} - The results of a configuration.
+   */
+  getContent(config: Object): Promise<Array<Object> | Error> {
+    // Or use the HttpService to get  content from web api's or websites.
+    return [{
+      key: 'value'
+    }]
+  }
+
+}
