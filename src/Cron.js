dea843b60ae512c14d90cc692a92e36d4b26c646
Chris Alderson <chrsalderson@protonmail.com>
2017-12-14 22:52:16 +0100

    feat: Allow cron to start at creation

--- src/Cron.js
+++ src/Cron.js
@@ -20,9 +20,11 @@ export default class Cron {
    * @param {!Object} [options={}] - The options for the Cron middleware.
    * @param {!string} [options.cronTime=0 0 *\/6 * * *] - The cron tab to
    * execute the scraper.
+   * @param {?boolean} [options.start=false] - Start the cronjob on creation.
    */
   constructor(PopApi: any, {
-    cronTime = '0 0 */6 * * *'
+    cronTime = '0 0 */6 * * *',
+    start = false
   }: Object = {}): void {
     /**
      * The cron time for scraping audios. Default is `0 0 *\/6 * * *`.
@@ -30,7 +32,7 @@ export default class Cron {
      */
     this.cronTime = cronTime
 
-    PopApi.cron = this.getCron(PopApi)
+    PopApi.cron = this.getCron(PopApi, start)
   }
 
   /**
